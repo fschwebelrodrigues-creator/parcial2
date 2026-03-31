13382164428c24595b3b07f8a9703e65df294cef
Schwebel <fschwebelrodrigues@gmail.com>
2026-03-31 08:04:57 -0300

    Create desafio5.py

---
+++ desafio5.py
@@ -0,0 +1,11 @@
+# float() → permite valores com vírgula (ex: 2.5)
+
+base = float(input("Digite a base do triângulo: "))
+altura = float(input("Digite a altura do triângulo: "))
+
+# Fórmula: área = (base * altura) / 2
+# * → multiplicação
+# / → divisão
+area = (base * altura) / 2
+
+print("A área do triângulo é:", area)
