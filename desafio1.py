53b27a94e42594cf8f2867d6f0485ac1732ba3a2
Schwebel <fschwebelrodrigues@gmail.com>
2026-03-31 07:59:45 -0300

    Create desafio1.py

---
+++ desafio1.py
@@ -0,0 +1,11 @@
+# input() → função que recebe dados do usuário (sempre como TEXTO / string)
+# float() → converte o texto digitado em número decimal (ex: 2.5, 3.0)
+
+num1 = float(input("Digite o primeiro número: "))  # recebe o valor e converte para número
+num2 = float(input("Digite o segundo número: "))   # recebe outro valor
+
+# + → operador de soma
+soma = num1 + num2  # variável "soma" armazena o resultado
+
+# print() → exibe o resultado na tela
+print("A soma é:", soma)
